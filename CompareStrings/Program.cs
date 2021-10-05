08f2cb23184dc31ef4fc2dadf93782676a4a31c6
Opilane TTHK <opilane@tthk.ee>
2021-10-05 14:06:35 +0300

    strings

---
+++ CompareStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CompareStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sisestada eesnime ja perekonnanime
+            //programm võrdleb sisetatud andmeid oma vahel
+            //programm kuvab konsoolis, kumb on pikem-eesnimi või perekonnanimi
+
+            Console.WriteLine("Sisesta oma nimi:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Sisesta oma perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            if (firstName.Length > lastName.Length)
+            {
+                Console.WriteLine("eesnimi on pikem kui perekonnanimi");
+            }
+            else if (firstName.Length < lastName.Length)
+            {
+                Console.WriteLine("perekonnanimi on pikem kui eesnimi");
+            }
+        }
+    }
+}
---
+++ StringsExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string helloWorld = "Hello World!";
+
+            //programm kuvab konsoolis sõna esimest tähte ja viimast tähte
+
+            Console.WriteLine(helloWorld[0]);
+            Console.WriteLine(helloWorld[helloWorld.Length-1]);
+        }
+    }
+}
